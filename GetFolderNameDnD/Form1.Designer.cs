ce0960b4c4c675a14199d935a23a0707bc5aace7
nsm01l <nsm01l@hotmail.com>
2024-10-28 22:21:39 +0900

    プロジェクト追加

---
+++ GetFolderNameDnD/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace GetFolderNameDnD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblFolder = new System.Windows.Forms.Label();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.lblDnD = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Folder:";
+            // 
+            // lblFolder
+            // 
+            this.lblFolder.AutoSize = true;
+            this.lblFolder.Location = new System.Drawing.Point(58, 9);
+            this.lblFolder.Name = "lblFolder";
+            this.lblFolder.Size = new System.Drawing.Size(49, 12);
+            this.lblFolder.TabIndex = 1;
+            this.lblFolder.Text = "lblFolder";
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(105, 35);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 2;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // lblDnD
+            // 
+            this.lblDnD.AllowDrop = true;
+            this.lblDnD.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblDnD.Location = new System.Drawing.Point(16, 71);
+            this.lblDnD.Name = "lblDnD";
+            this.lblDnD.Size = new System.Drawing.Size(256, 181);
+            this.lblDnD.TabIndex = 3;
+            this.lblDnD.Text = "DnD Here";
+            this.lblDnD.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblDnD.DragDrop += new System.Windows.Forms.DragEventHandler(this.lblDnD_DragDrop);
+            this.lblDnD.DragEnter += new System.Windows.Forms.DragEventHandler(this.lblDnD_DragEnter);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblDnD);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.lblFolder);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "GetFolderNameDnD";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblFolder;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Label lblDnD;
+    }
+}
+
---
+++ GetFolderNameDnD/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+
+namespace GetFolderNameDnD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 起動時の初期化処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblFolder.Text = "";
+            btnOK.Enabled = false;
+            Clipboard.Clear();
+        }
+
+        /// <summary>
+        /// ドラッグ&ドロップされた
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void lblDnD_DragDrop(object sender, DragEventArgs e)
+        {
+            var files = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            if (files.Length == 1)
+            {
+                if (Directory.Exists(files[0]))
+                {
+                    lblFolder.Text = files[0];
+                    btnOK.Enabled = true;
+                }
+                else
+                {
+                    btnOK.Enabled = false;
+                }
+            }
+            else
+            {
+                btnOK.Enabled = false;
+            }
+        }
+
+        /// <summary>
+        /// D&D しようとマウスカーソルが来た
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void lblDnD_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.All;
+        }
+
+        /// <summary>
+        /// OK が押された
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(lblFolder.Text);
+            this.Close();
+        }
+
+        /// <summary>
+        /// ×が押された
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+        }
+    }
+}
